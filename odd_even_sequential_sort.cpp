a5f58503c6cbdd3664d2e77efb0ac1085d181c77
Jiaqi Li <120090727@link.cuhk.edu.cn>
2022-10-03 10:32:35 +0800

    sequential sort implem ok

--- odd_even_sequential_sort.cpp
+++ odd_even_sequential_sort.cpp
@@ -3,6 +3,11 @@
 #include <iostream>
 #include <chrono>
 
+// A helper function that determines whether the input number is even
+inline bool is_even(int num) {
+    return (num % 2) == 0;
+}
+
 int main (int argc, char **argv){
 
     int num_elements; // number of elements to be sorted
@@ -25,16 +30,47 @@ int main (int argc, char **argv){
     std::chrono::duration<double> time_span;
     t1 = std::chrono::high_resolution_clock::now(); // record time
 
-    /* TODO BEGIN
-        Implement sequential odd even transportation sort
-        Code in this block is not a necessary. 
-        Replace it with your own code.
-    */
+    /* TODO BEGIN */
    // The array is stored in `elements`
+    for (int iter = 0; iter < num_elements; iter++) {  // in total need to perform n times to finish the sorting
+        for (int i = 0; i < num_elements; i++) {    // inside each iteration
+            // Determine whether the iteration number is odd or even
+            // We assume iteration number starts from 0
+            if (is_even(i)) {
+                // In even iteration, do odd-even exchange
+
+                // For each odd index item, compare with its preceding item and do sort among these 2
+                for (int j = 1; j < num_elements; j += 2) {
+                    if (elements[j] < elements[j-1]) {
+                        // Do swap when the two are not in ascending order
+                        int tmp = elements[j];
+                        elements[j] = elements[j-1];
+                        elements[j-1] = tmp;
+                    }
+                }
+            } else {
+                // In odd iteration, do even-odd exchange
+
+                // For each even index item, compare with its preceding item and do sort among them
+                for (int k = 0; k < num_elements; k += 2) {
+                    if (elements[k] < elements[k-1]) {
+                        // Do swap when the two are not in ascending order
+                        int tmp = elements[k];
+                        elements[k] = elements[k-1];
+                        elements[k-1] = tmp;
+                    }
+                }
+            }
+        }
+    }
 
     for (int i = 0; i < num_elements; i++) {
         sorted_elements[i] = elements[i];
     }
+   // Print out my info
+   printf("Name: Li Jiaqi\n");
+   printf("Student ID: 120090727\n");
+   printf("Assignment 1, Odd-Even Transposition Sort, Sequential Version.\n");
     /* TODO END */
 
     t2 = std::chrono::high_resolution_clock::now();  
