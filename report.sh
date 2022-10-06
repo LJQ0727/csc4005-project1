5c9d56fdcaff4043cd07a45e281ed9c9bfd65fad
Jiaqi Li <120090727@link.cuhk.edu.cn>
2022-10-05 22:15:59 -0400

    update reportign

--- report.sh
+++ report.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=parallel_job_jiaqi # Job name
-#SBATCH --nodes=4                    # Run all processes on a single node	
+#SBATCH --nodes=2                    # Run all processes on a single node	
 #SBATCH --ntasks=20                  # number of processes = 4
 #SBATCH --cpus-per-task=4            # Number of CPU cores per process
 #SBATCH --mem=600mb                  # Total memory limit
@@ -14,16 +14,26 @@ make num_procs=1 parallel
 make test
 make num_procs=2 parallel
 make test
-make num_procs=3 parallel
-make test
 make num_procs=4 parallel
 make test
-make num_procs=5 parallel
+make num_procs=6 parallel
+make test
+make num_procs=8 parallel
 make test
 make num_procs=10 parallel
 make test
-make num_procs=15 parallel
+make num_procs=12 parallel
+make test
+make num_procs=14 parallel
+make test
+make num_procs=16 parallel
+make test
+make num_procs=18 parallel
 make test
 make num_procs=20 parallel
+make test
+make num_procs=22 parallel
+make test
+make num_procs=24 parallel
 make test
 
